ecd3610886d01ef8e2467e7bec9324fa323147fd
Derek Lewis <DerekNonGeneric@inf.is>
2020-02-19 23:44:26 -0500

    🧪✨ Verify device satisfies specified node version

---
+++ test/test-node-version.mjs
@@ -0,0 +1,18 @@
+import { createRequire } from 'module';
+import { PATHS } from 'loader339/constants';
+
+const require = createRequire(import.meta.url);
+const pjsonObj = require(PATHS.manifest);
+const semver = require('semver');
+const tap = require('tap');
+
+// Accommodate for labels and build metadata appearing as SemVer extensions.
+const NODE_VERSION = process.version.split('-')[0];
+
+tap.test((t) => {
+  t.ok(
+    semver.satisfies(NODE_VERSION, pjsonObj.engines.node),
+    `current \`node\`, ${NODE_VERSION}, must satisfy ${pjsonObj.engines.node}`,
+  );
+  t.done();
+});
