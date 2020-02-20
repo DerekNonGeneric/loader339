42ec12ec83d78b42aa7c479172b4a0d1486eede9
Derek Lewis <DerekNonGeneric@inf.is>
2020-02-19 22:22:58 -0500

    🧪⚙️ Get test working with updated project name

--- test/test-constants.mjs
+++ test/test-constants.mjs
@@ -1,12 +1,8 @@
+import { projectRoot } from 'loader339/constants';
 import tap from 'tap';
-import { resolve } from 'path';
-
-import { projectRoot } from 'lorem/constants'
-
-const root = '/'
 
 tap.test(t => {
-  t.ok(projectRoot.includes('lorem-demo'), 'path should include lorem-demo');
+  t.ok(projectRoot.includes('loader339'), 'path should include loader339');
   t.ok(!projectRoot.includes('lib'), 'it should not include lib');
   t.done();
 });
