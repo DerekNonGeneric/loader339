69ef4ffb44e43d41ed486c15688c8766ca4a7993
Derek Lewis <DerekNonGeneric@inf.is>
2020-02-19 22:30:36 -0500

    🏗️♻️  Prepare for constants other than paths

--- test/test-constants.mjs
+++ test/test-constants.mjs
@@ -1,8 +1,8 @@
-import { projectRoot } from 'loader339/constants';
+import { PATHS } from 'loader339/constants';
 import tap from 'tap';
 
 tap.test(t => {
-  t.ok(projectRoot.includes('loader339'), 'path should include loader339');
-  t.ok(!projectRoot.includes('lib'), 'it should not include lib');
+  t.ok(PATHS.projectRoot.includes('loader339'), 'path should include loader339');
+  t.ok(!PATHS.projectRoot.includes('lib'), 'it should not include lib');
   t.done();
 });
